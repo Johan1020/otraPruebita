9865cca8d9dc74115600e5ec1bff7a2e6195520d
Johan1020 <londonojohan02@gmail.com>
2024-09-11 15:46:59 -0500

    :)

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -2,10 +2,38 @@ import React from 'react';
 
 const Home = () => {
   return (
+    <> 
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
     </div>
+
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </>
   );
 };
 
